f7064a88db3db4213c3bbba563b7bb913745eadb
cardcash <terencecawleynew@gmail.com>
2026-06-21 11:55:45 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { makeBadge } = require('badge-maker');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/badge/:data', (req, res) => {
+    // Splits "label-message" from URL
+    const [label, message] = req.params.data.split('-');
+
+    const svg = makeBadge({
+        label: label || 'const',
+        message: message || 'badges',
+        color: req.query.color || 'blue',
+        style: req.query.style || 'flat'
+    });
+
+    res.setHeader('Content-Type', 'image/svg+xml');
+    res.send(svg);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`const-badges online at port ${PORT}`));
